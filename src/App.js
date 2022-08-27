04551591ffcfed7c5b9d0ebd143f739318957858
Bikesh Rai <bikeshrai78@gmail.com>
2022-08-27 20:00:21 +0545

    Add task

--- src/App.js
+++ src/App.js
@@ -1,18 +1,50 @@
+import { useReducer } from "react";
 import Header from "./Components/Header";
 import Sidebar from "./Components/Sidebar";
 import TaskContainer from "./Components/TaskContainer";
+import { AppContext } from "./Context/AppContext";
 import "./style.scss";
 
 function App() {
+  // const initialState = {
+  //   taskList: JSON.stringify(localStorage.getItem("gtdTask")) || [],
+  // };
+  const initialState = {
+    taskList: [
+      {
+        id: 1,
+        taskName: "This is test name",
+        takDetail: "",
+        projectName: "Get healthy",
+        startDate: "",
+        endDate: "",
+        isCompleted: false,
+        context: "",
+        givenTo: "",
+      },
+    ],
+  };
+  const reducer = (state, action) => {
+    switch (action.type) {
+      case "addTask":
+        // localStorage.setItem("gtdTask", state.taskList);
+        return { ...state, taskList: [...state.taskList, action.payload] };
+      default:
+        throw new Error(`${action.type} action is not defined`);
+    }
+  };
+  const [state, dispatch] = useReducer(reducer, initialState);
   return (
     <>
       <Header />
-      <main>
-        <div className="main-content">
-          <Sidebar />
-          <TaskContainer />
-        </div>
-      </main>
+      <AppContext.Provider value={{ state, dispatch }}>
+        <main>
+          <div className="main-content">
+            <Sidebar />
+            <TaskContainer />
+          </div>
+        </main>
+      </AppContext.Provider>
     </>
   );
 }
--- src/Components/TaskContainer.js
+++ src/Components/TaskContainer.js
@@ -1,26 +1,85 @@
-import React from "react";
+import React, { useContext, useState } from "react";
+import { AppContext } from "../Context/AppContext";
+import EditBar from "./EditBar";
 
 const TaskContainer = () => {
+  const { state, dispatch } = useContext(AppContext);
+  const { taskList } = state;
+  const [showEditBar, setShowEditBar] = useState(false);
+  const [taskInput, setTaskInput] = useState("");
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    dispatch({
+      type: "addTask",
+      payload: {
+        id: new Date().getMilliseconds(),
+        taskName: taskInput,
+        takDetail: "",
+        projectName: "",
+        startDate: "",
+        endDate: "",
+        isCompleted: false,
+        context: "",
+        givenTo: "",
+      },
+    });
+    console.log("state", state);
+  }
+  console.log("state", state);
   return (
     <div className="task-container">
       <div className="task-container-head">
         <div className="task-head-title">Completed</div>
         <div className="task-head-title">Incompleted</div>
       </div>
-      <form className="task-form">
+      <form className="task-form" onSubmit={handleSubmit}>
         <div className="input-container">
-          <input type="text" placeholder="Add a task" />
+          <input
+            type="text"
+            placeholder="Add a task"
+            value={taskInput}
+            onChange={(e) => setTaskInput(e.target.value)}
+          />
           <button className="btn-primary">Add</button>
         </div>
         <div className="task-lists">
-          {/* <input type="text" value="Create Sidebar" /> */}
-          <div className="task-list">
+          {/* <div className="task-list" onClick={(e) => setShowEditBar(true)}>
             <input type="checkbox" name="" id="" />
             <p className="task">Create Sidebar</p>
-            <button className="btn-primary">Edit</button>
-          </div>
+            <div className="button-container">
+              <button
+                className="btn-primary"
+                onClick={(e) => setShowEditBar(true)}
+              >
+                Edit
+              </button>
+              <button className="btn-primary">Delete</button>
+            </div>
+          </div> */}
+          {taskList &&
+            taskList.map((task) => (
+              <div
+                key={task.id}
+                className="task-list"
+                onClick={(e) => setShowEditBar(true)}
+              >
+                <input type="checkbox" name="" id="" />
+                <p className="task">{task.taskName}</p>
+                <div className="button-container">
+                  <button
+                    className="btn-primary"
+                    onClick={(e) => setShowEditBar(true)}
+                  >
+                    Edit
+                  </button>
+                  <button className="btn-primary">Delete</button>
+                </div>
+              </div>
+            ))}
         </div>
       </form>
+      {showEditBar && <EditBar />}
     </div>
   );
 };
